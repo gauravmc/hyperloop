15458f00902682eae208438dad369442d8685dfa
Gaurav Chande <gmail@gauravchande.com>
2013-08-10 20:00:04 -0400

    Refactor server processing a little bit

--- hyperloop.rb
+++ hyperloop.rb
@@ -8,17 +8,26 @@ class Hyperloop
 
   def start
     loop do
-      socket = @server.accept
-
-      data = socket.readpartial 1024
-      puts data
-
-      socket.write "HTTP/1.1 200 OK\r\n"
-      socket.write "\r\n"
-      socket.write "w00t\n"
-
-      socket.close
+      @socket = @server.accept
+      read_data_from_socket
+      send_response
+      close_socket
     end
+  end
+
+  def read_data_from_socket
+    data = @socket.readpartial 1024
+    puts data
+  end
+
+  def send_response
+    @socket.write "HTTP/1.1 200 OK\r\n"
+    @socket.write "\r\n"
+    @socket.write "w00t\n"
+  end
+
+  def close_socket
+    @socket.close
   end
 end
 
